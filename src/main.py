8f0f05c0cdd13985d1fbc3738d70602a249df738
Mariusz Kryński <mrk@sed.pl>
2023-02-13 19:57:18 +0100

    read defaults from python-can config

--- src/main.py
+++ src/main.py
@@ -3,19 +3,24 @@ import asyncio
 import logging
 
 import can2mqtt
+import can
 
 
 def main():
+    config = can.util.load_config()
+    default_mqtt_server = config.get("mqtt_server", "localhost")
+    default_mqtt_topic = config.get("mqtt_topic", "homeassistant")
+
     parser = argparse.ArgumentParser(
         prog="can2mqtt",
         description="CAN to MQTT converter",
     )
-    parser.add_argument("-s", "--mqtt-server", default="localhost")
+    parser.add_argument("-s", "--mqtt-server", default=default_mqtt_server)
     parser.add_argument("-i", "--interface")
     parser.add_argument("-c", "--channel")
     parser.add_argument("-b", "--bitrate", type=int, default=125000)
     parser.add_argument("-l", "--log-level", default=logging.INFO)
-    parser.add_argument("-t", "--mqtt-topic-prefix", default="homeassistant")
+    parser.add_argument("-t", "--mqtt-topic-prefix", default=default_mqtt_topic)
     args = parser.parse_args()
 
     logging.basicConfig(level=args.log_level)
