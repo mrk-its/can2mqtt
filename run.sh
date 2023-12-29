1abbc2b853ff05f466de816d3b0059133e5ee714
Mariusz Kryński <mrk@samba.tv>
2023-12-29 21:44:46 +0100

    bugfix, bump rev

--- run.sh
+++ run.sh
@@ -5,8 +5,7 @@ BITRATE=$(bashio::config 'bitrate')
 SERVER=$(bashio::config 'mqtt_server')
 TOPIC=$(bashio::config 'mqtt_topic_prefix')
 TOPIC=$(test -n "$TOPIC" && echo "-t $TOPIC")
-TIMEOUT=$(bashio::config 'sdo_response_timeout')
-TIMEOUT=$(test -n "$TIMEOUT" -a "$TIMEOUT" != null && echo "--sdo-response-timeout $TIMEOUT")
+TIMEOUT=$(bashio::config 'sdo_response_timeout' 2.0)
 
 set -x
-/app/venv/bin/can2mqtt -i "$INTERFACE" -s "$SERVER" -c "$CHANNEL" -b "$BITRATE" $TOPIC $TIMEOUT
+/app/venv/bin/can2mqtt -i "$INTERFACE" -s "$SERVER" -c "$CHANNEL" -b "$BITRATE" $TOPIC --sdo-response-timeout $TIMEOUT
