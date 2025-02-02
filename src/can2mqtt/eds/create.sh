802b171f8a6526b0a439795bbdc3a91b9d545c41
Mariusz Kryński <mrk@sed.pl>
2025-02-02 21:13:25 +0100

    generate TPDO OD entries

--- src/can2mqtt/eds/create.sh
+++ src/can2mqtt/eds/create.sh
@@ -3,9 +3,4 @@ EDSFILE=$DIR/esphome.eds
 
 (cat $DIR/header.eds.tpl; echo) > $EDSFILE
 
-for TPDO in `seq 0 7`; do
-  TPDO=$TPDO envsubst < $DIR/tpdo.eds.tpl ;
-  echo
-done >> $EDSFILE
-
 (cat $DIR/entity_header.eds.tpl; echo) >> $EDSFILE
